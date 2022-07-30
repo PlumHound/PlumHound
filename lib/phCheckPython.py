056f62c2aa8c967608c8e5d1103610099c89af39
Kent Ickler <kent@defensiveorigins.com>
2022-07-30 00:07:59 -0600

    Update phCheckPython.py
    update WARN on python 3.10

--- lib/phCheckPython.py
+++ lib/phCheckPython.py
@@ -10,3 +10,9 @@ def CheckPython():
     if sys.version_info < (3, 0, 0):
         print(__file__ + ' requires Python 3, while Python ' + str(sys.version[0] + ' was detected. Terminating. '))
         sys.exit(1)
+        
+    if sys.hexversion > 0x3090000:
+        print()
+        print('WARN: Python hexversion %s is in use.' % hex(sys.hexversion))
+        print('WARN: Plumhound may have unexpected behavior. Use > 0x3000000 < 0x3100000')
+        print()
